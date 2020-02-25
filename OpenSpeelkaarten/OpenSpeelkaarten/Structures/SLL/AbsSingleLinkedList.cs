c7c81f4e2d16a1a89f4718e75043c1b7f85b2e1c
Ricardo Stam <Ricardo.stam@protonmail.ch>
2020-02-25 22:12:55 +0100

    Added Insertions of SLL.

--- OpenSpeelkaarten/OpenSpeelkaarten/Structures/SLL/AbsSingleLinkedList.cs
+++ OpenSpeelkaarten/OpenSpeelkaarten/Structures/SLL/AbsSingleLinkedList.cs
@@ -10,19 +10,44 @@ namespace OpenSpeelkaarten.Structures.SLL
         // Insertions
         public void Insert(T value)
         {
-            throw new NotImplementedException();
+            head = new SLLNode<T>(value, head.GetNext());
         }
         public void InsertAtEnd(T value)
         {
-            throw new NotImplementedException();
+            if (head.IsEmpty()) Insert(value);
+            else
+            {
+                ISingleLinkedListNode<T> temp = head;
+                while (!temp.GetNext().IsEmpty()) temp = temp.GetNext();
+                temp.SetTail(new SLLNode<T>(value, temp.GetNext()));
+            }
         }
         public void InsertAfter(T value, T targetValue)
         {
-            throw new NotImplementedException();
+            if (head.IsEmpty()) throw new Exception("The List is empty!");
+            else
+            {
+                ISingleLinkedListNode<T> temp = head;
+                while (!temp.IsEmpty() && !Equals(temp.GetValue(), targetValue)) temp = temp.GetNext();
+                if (temp.IsEmpty()) throw new Exception("The value is not present within the list.");
+                temp.SetTail(new SLLNode<T>(value, temp.GetNext()));
+            }
         }
         public void InsertBefore(T value, T targetValue)
         {
-            throw new NotImplementedException();
+            if (head.IsEmpty()) throw new Exception("The list is empty!");
+            else if (Equals(head.GetValue(), targetValue)) Insert(value);
+            else
+            {
+                ISingleLinkedListNode<T> actual = head, before = head;
+                while(!actual.IsEmpty() && !Equals(actual.GetValue(), targetValue))
+                {
+                    before = actual;
+                    actual = actual.GetNext();
+                }
+                if (actual.IsEmpty()) throw new Exception("The value is ot present in the list.");
+                before.SetTail(new SLLNode<T>(value, actual));
+            }
         }
 
         // Deletion
